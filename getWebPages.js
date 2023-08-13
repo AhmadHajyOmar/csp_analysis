eeefd2014bfecb4986fbab4a597ea9b1eeb7ebb7
Ahmad Hajy Omar <ahm2d1z8dr@gmail.com>
2023-08-14 00:21:40 +0200

    ch2

---
+++ getWebPages.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+var websites = fs.readFileSync("./top-1m.csv", 'utf-8').split(/\r?\n/);
+const options = process.argv.splice(2);
+var numbreOfWebsites = 1000000;
+if(options.length > 0){
+    numbreOfWebsites = parseInt(options[0]);
+}
+var websitesURLS = new String();
+for(var w = 0; w < numbreOfWebsites; w++){
+    websitesURLS += `https://www.${websites[w].split(",")[1]}.com`+"\n";
+}
+
+websitesURLS = websitesURLS.replace(/\n+$/, '');
+console.log(websitesURLS);
+fs.writeFileSync(`./tests/websites.txt`, websitesURLS)
